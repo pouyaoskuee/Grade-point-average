3c4184e0f8e584547a8d6bd872d79fccbe64e05a
pouya <pouyakhaleghi82@gmail.com>
2024-08-09 01:28:58 +0330

    start

---
+++ script.py
@@ -0,0 +1,20 @@
+num1=int(input("nomre avali"))#1
+v1=int(input("vahed avali"))
+num2=int(input("nomre fizik"))#2
+
+num3=int(input("nomre shimi"))#3
+
+num4=int(input("nomre zist"))#4
+
+num5=int(input("nomre tarikh"))#5
+
+num6=int(input("nomre maaref"))#6
+
+r=(num1*3)
+i=(b*3)
+s=(c*3)
+z=(d*4)
+t=(e*2)
+m=(f*2)
+jam=(r+i+s+z+t+m)
+print(jam/17)
